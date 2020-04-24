620ecd090eeb050c728a2d860287483a24639991
Butler925 <butler925@gmail.com>
2020-04-24 13:46:48 -0400

    Updated website function and visualization

--- dquest-flask/app/lib/ctgov.py
+++ dquest-flask/app/lib/ctgov.py
@@ -2,127 +2,176 @@
 set of function to interact with ClinicalTrials.gov
 '''
 
-from app import app
+from app import app, general_pool_criteria
 from web import download_web_data
 from xml.etree import ElementTree
+from pyzipcode import ZipCodeDatabase
 from extensions import cache
 import oformat as of
 import urllib
 
+
 # global ctgov search
-def get_initial_nct (txt):
-    txt = of.format_search_terms (txt)
+def get_initial_nct(txt):
+    txt = of.format_search_terms(txt)
     url = 'http://clinicaltrials.gov/search?cond=%s&displayxml=true' % txt
-    return get_initial_nct_from_url (url)
+    return get_initial_nct_from_url(url)
 
-def get_initial_nct_detail (recrs,cond,locn):
-    recrs = of.format_search_terms (recrs)
-    cond = of.format_search_terms (cond)
-    locn = of.format_search_terms (locn)
-    url = 'http://clinicaltrials.gov/search?cond=%s&recrs=%s&locn=%s&displayxml=true' % (cond,recrs,locn)
+
+def get_initial_nct_detail(recrs, cond, locn):
+    recrs = of.format_search_terms(recrs)
+    cond = of.format_search_terms(cond)
+    locn = of.format_search_terms(locn)
+    url = 'http://clinicaltrials.gov/search?cond=%s&recrs=%s&locn=%s&displayxml=true' % (cond, recrs, locn)
     print(url)
-    return get_initial_nct_from_url (url)
+    return get_initial_nct_from_url(url)
 
-def get_initial_nct_patient (cond,locn):
-    cond = of.format_search_terms (cond)
-    locn = of.format_search_terms (locn)
+
+def get_initial_nct_patient(cond, locn):
+    cond = of.format_search_terms(cond)
+    locn = of.format_search_terms(locn)
     # url = 'http://clinicaltrials.gov/search?cond=%s&recrs=recrs&type=intr&locn=%s&displayxml=true' % (cond,locn)
     url = 'http://clinicaltrials.gov/search?cond=%s&type=intr&locn=%s&displayxml=true' % (cond, locn)
     print(url)
-    return get_initial_nct_from_url (url)
+    return get_initial_nct_from_url(url)
 
+
+def get_nct_list_from_zip(input_zip):
+    zcdb = ZipCodeDatabase()
+    zip_list = [z.zip for z in zcdb.get_zipcodes_around_radius(input_zip, 100)]  # default mile range set at 100
+
+    conn = general_pool_criteria.connection()
+    cur = conn.cursor()
+    sql = '''
+            ;with cte (code, DeclarationItem, Declaration) as
+            (
+              select nct_id,
+                cast(left(zip_codes, charindex('|',zip_codes+'|')-1) as varchar(50)) DeclarationItem,
+                     stuff(zip_codes, 1, charindex('|',zip_codes+'|'), '') Declaration
+              from dbo.aact_trial_info
+              union all
+              select code,
+                cast(left(Declaration, charindex('|',Declaration+'|')-1) as varchar(50)) DeclarationItem,
+                stuff(Declaration, 1, charindex('|',Declaration+'|'), '') Declaration
+              from cte
+              where Declaration > ''
+            ) 
+            select code as nct_id, DeclarationItem as zip
+            from cte
+            order by nct_id;
+        '''
+    cur.execute(sql)
+    trial_zips = cur.fetchall()
+    conn.close()
+    cur.close()
+
+    # compare nearby zip codes to trial zip codes
+    nearby_nct_list = []
+    for item in trial_zips:
+        test_nct = item[0]
+        test_zip = item[1]
+        if test_zip in zip_list:
+            nearby_nct_list.append(test_nct)
+    nearby_nct_list = list(set(nearby_nct_list))
+    # temp loop to add trial number, to be removed in the future
+    temp_return_list = []
+    i = 1
+    for item in nearby_nct_list:
+        temp_return_list.append('%s;%d' % (item, i))
+        i += 1
+    return temp_return_list;
 
 
 # global ctgov search from url
-@cache.memoize(604800)
-def get_initial_nct_from_url (url):
+# @cache.memoize(604800)
+def get_initial_nct_from_url(url):
     # num. of studies available
     n = get_nct_number('%s&count=0' % url)
     print('num. of studies available ', n)
     if n == 0:
         return []
     # get the list of clinical studies
-    xmltree = ElementTree.fromstring (download_web_data('%s&count=%d' % (url, n)))
-    lnct = xmltree.findall ('clinical_study')
+    xmltree = ElementTree.fromstring(download_web_data('%s&count=%d' % (url, n)))
+    lnct = xmltree.findall('clinical_study')
     rnct = []
     i = 1
     for ct in lnct:
-        ids = ct.find ('nct_id')
+        ids = ct.find('nct_id')
         if ids is None:
             continue
-        rnct.append ('%s;%d' % (ids.text,i))
+        rnct.append('%s;%d' % (ids.text, i))
         i += 1
     print(rnct)
     return rnct
 
 
 # get result number
-def get_nct_number (url):
+def get_nct_number(url):
     xml = download_web_data(url)
     if xml is None:
         return 0
-    xmltree = ElementTree.fromstring (xml)
-    nnct = xmltree.get ('count')
+    xmltree = ElementTree.fromstring(xml)
+    nnct = xmltree.get('count')
     return int(nnct)
 
 
 # parse clinical trial details
-def parse_xml_nct (ct):
-    ids = ct.find ('nct_id')
+def parse_xml_nct(ct):
+    ids = ct.find('nct_id')
     if ids is None:
         return ids
     ids = ids.text
-    rank = ct.find ('order')
+    rank = ct.find('order')
     if rank is not None:
         rank = rank.text
-    title = ct.find ('title')
+    title = ct.find('title')
     if title is not None:
         title = title.text
-    condition = ct.find ('condition_summary')
+    condition = ct.find('condition_summary')
     if condition is not None:
         condition = condition.text
     return (ids, rank, title, condition)
 
 
 # ctgov search
-def search (txt, npag):
-    txt = of.format_search_terms (txt)
+def search(txt, npag):
+    txt = of.format_search_terms(txt)
     url = 'http://clinicaltrials.gov/search?cond=%s&displayxml=true' % txt
-    return retrieve_trials (url, npag)
+    return retrieve_trials(url, npag)
 
 
 # ctgov advanced search
 def advanced_search(param):
-    url = form_advanced_search_url (param)
-    return retrieve_trials (url, param.get('npag'))
+    url = form_advanced_search_url(param)
+    return retrieve_trials(url, param.get('npag'))
 
 
 # get advanced search url
-def form_advanced_search_url (param):
+def form_advanced_search_url(param):
     ctg_param = ''
     for r in sorted(param):
-        k = urllib.quote(r,'')
+        k = urllib.quote(r, '')
         if k == 'qlabel':
             continue
         for v in param.getlist(r):
             ctg_param += "%s=%s&" % (k, urllib.quote(v, ''))
     return 'http://clinicaltrials.gov/ct2/results?%sdisplayxml=True' % ctg_param
-    
+
 
 # get the list of resulting clinical trials
-def retrieve_trials (url, npag):
+def retrieve_trials(url, npag):
     # num. of studies available
     n = of.format_nct_number(get_nct_number('%s&count=0' % url))
     # get the list of clinical studies
     xml = download_web_data('%s&pg=%s' % (url, npag))
     if xml is None:
         return (0, [])
-    xmltree = ElementTree.fromstring (xml)
-    lnct = xmltree.findall ('clinical_study')
+    xmltree = ElementTree.fromstring(xml)
+    lnct = xmltree.findall('clinical_study')
     nct = []
     for ct in lnct:
-        pct = parse_xml_nct (ct)
+        pct = parse_xml_nct(ct)
         if pct[0] is not None:
-            cond = of.format_condition (pct[3])
-            nct.append ((pct[0], pct[1], pct[2], cond))
+            cond = of.format_condition(pct[3])
+            nct.append((pct[0], pct[1], pct[2], cond))
     return (n, nct)
--- dquest-flask/app/lib/question_info_entropy.py
+++ dquest-flask/app/lib/question_info_entropy.py
@@ -1,4 +1,6 @@
-from app import general_pool_criteria,general_pool_aact
+from app import general_pool_criteria, general_pool_aact
+
+
 
 
 def filter_nct_ids_by_pre_questions(answer_list):
@@ -9,17 +11,17 @@ def filter_nct_ids_by_pre_questions(answer_list):
     '''
     answer_list = [str(x) for x in answer_list]
     age = answer_list[0]
-    gender = answer_list[1]
+    # gender = answer_list[1]
+    exposure = answer_list[1]
     domain = answer_list[2]
     user_picked_time = answer_list[3]
-    exposure = answer_list[4]
-    stat = answer_list[5]
-    preg = answer_list[6]
+    stat = answer_list[4]
+    preg = answer_list[5]
     # query database filter nctids
     conn = general_pool_criteria.connection()
     cur = conn.cursor()
-    params = [age, age, age, gender, gender, domain, domain, domain, domain, exposure, exposure, exposure, stat, stat,
-              stat, preg, preg, preg, preg]
+    params = [age, age, domain, domain, domain, domain, exposure, exposure, exposure, stat, stat,
+               stat, preg, preg, preg, preg, user_picked_time, user_picked_time, user_picked_time, user_picked_time]
     placeholder1 = ",".join("?" * len(params))
 
     sql = '''
@@ -31,13 +33,13 @@ def filter_nct_ids_by_pre_questions(answer_list):
                 (%s >= aact.minimum_age or aact.minimum_age is null) 
             and 
                 (%s <= aact.maximum_age or aact.maximum_age is null) 
-            and
-               (1= case 
-                        when '%s' = 'male' and aact.gender in ('male', 'all') then 1 
-                        when '%s' = 'female' and aact.gender in ('female', 'all') then 1 
-                        when '%s' = 'other' and aact.gender in ('male', 'female', 'all') then 1
-                            else 0 
-                    end)
+            -- and
+            --    (1= case 
+            --             when s = 'male' and aact.gender in ('male', 'all') then 1 
+            --             when s = 'female' and aact.gender in ('female', 'all') then 1 
+            --             when s = 'other' and aact.gender in ('male', 'female', 'all') then 1
+            --                 else 0 
+            --         end)
              and
                 (1= case
                     when '%s' = 'yes' and  keyc.disease_status in ('yes', 'all') then 1
@@ -71,12 +73,13 @@ def filter_nct_ids_by_pre_questions(answer_list):
                 1= case
                     when ('%s' = 'None'  or '%s' = '') and keyc.days_to_disease is null then 1
                     when '%s' != 'None' and
+            --             ((CURRENT_DATE  - TO_DATE(s, 'MM/DD/YYYY'))
                         (DATEDIFF(day, CONVERT(VARCHAR, '%s', 101), CONVERT(VARCHAR, getdate(), 101)) 
                         <= keyc.days_to_disease or keyc.days_to_disease is null) then 1
                     else 0
                 end)
-        ''' % (age, age, gender, gender, gender, domain, domain, domain, domain, exposure, exposure, exposure, stat, stat,
-              stat, preg, preg, preg, preg, user_picked_time, user_picked_time, user_picked_time, user_picked_time)
+        ''' % (age, age, domain, domain, domain, domain, exposure, exposure, exposure, stat, stat,
+               stat, preg, preg, preg, preg, user_picked_time, user_picked_time, user_picked_time, user_picked_time)
 
     print(sql)
     cur.execute(sql)
@@ -91,19 +94,19 @@ def filter_nct_ids_by_pre_questions(answer_list):
     return result_ids
 
 
-def find_annotated_nct_id_list():
+def find_active_nct_id_list():
     '''
-        find annotated working nct id list
+        find annotated working nct id list which is actively recruiting
         :param: none
         :return: a working nct id list in our annotation list
     '''
-    annotated_working_list =[]
+    annotated_working_list = []
     conn = general_pool_criteria.connection()
     cur = conn.cursor()
-    sql ='''
+    sql = '''
         select distinct(nct_id)
-        from dbo.aact_trial_info
-        where status = 'Recruiting' and nct_id in ( select distinct(nct_id)
+        from dbo.aact_trial_info as aact
+        where status in ('Recruiting', 'Enrolling by invitation', 'Available') and nct_id in ( select distinct(nct_id)
                                                     from dbo.all_criteria );
     '''
     cur.execute(sql)
@@ -114,16 +117,17 @@ def find_annotated_nct_id_list():
     if len(nctids) > 0:
         for nctid in nctids:
             annotated_working_list.append(str(nctid[0]))
+
     return annotated_working_list
 
 
-
-def init_working_nct_id_list(rnct, pre_quest_answers = []):
+def init_working_nct_id_list(rnct, pre_quest_answers=[]):
     '''
     initialize working nct id list
     :param rnct: returned from ctgov search results [...,'NCT02733523;3431','NCT02075840;3432',...]
     :return: a working nct id list
     '''
+    print(rnct)
     working_nct_id_list = []
     if len(pre_quest_answers) > 0:
         # annotated_working_list = find_annotated_nct_id_list()
@@ -131,7 +135,6 @@ def init_working_nct_id_list(rnct, pre_quest_answers = []):
         print('in annotated: ', annotated_working_list)
 
         for record in rnct:
-            print(record.split(';')[0])
             if record.split(';')[0] in annotated_working_list:
                 working_nct_id_list.append([record.split(';')[0], int(record.split(';')[1]), 0])
     else:
@@ -139,19 +142,19 @@ def init_working_nct_id_list(rnct, pre_quest_answers = []):
     print('initial working list len: ', len(working_nct_id_list))
     return working_nct_id_list
 
-
 def init_working_nct_id_list_original(rnct):
     '''
     initialize working nct id list
     :param rnct: returned from ctgov search results [...,'NCT02733523;3431','NCT02075840;3432',...]
     :return: a working nct id list
     '''
-    working_nct_id_list  = [[record.split(';')[0], int(record.split(';')[1]), 0] for record in rnct]
+    working_nct_id_list = [[record.split(';')[0], int(record.split(';')[1]), 0] for record in rnct]
     return working_nct_id_list
+
 
 # working_nct_id_list = [['NCT02901717', 3431, 0], ['NCT01287182', 3432, 0],['NCT01035944', 3432, 0],['NCT00562068', 3431, 1], ['NCT00742300', 3431, 2]]
 # question_answer_list = [{'answer': {}, 'question': {'domain': 'condition', 'entity_text': 'pregnant'}} ]
-def find_new_question(question_answer_list,working_nct_id_list, domain='all'):
+def find_new_question(question_answer_list, working_nct_id_list, domain='all'):
     '''
     find new question by frequency.
     alternatively, information entropy should be considered sum(plog(p))
@@ -164,11 +167,12 @@ def find_new_question(question_answer_list,working_nct_id_list, domain='all'):
     question_answer_list = [{'answer': {}, 'question': (3, u'pregnant')}]
     '''
     # working_nct_id_frame = pd.DataFrame(working_nct_id_list,columns=['nct_id', 'ctgov_rank', 'num_of_question'])
-    working_nct_id_0 = [record[0] for record in working_nct_id_list if record[2] == 0]
-
+    working_nct_id_0 = [str(record[0]) for record in working_nct_id_list if record[2] == 0]
 
     working_nct_id_0_len = len(working_nct_id_0)
-    placeholders1 = ",".join("?" * working_nct_id_0_len)
+    placeholders1 = ",".join(str("'" + x + "'") for x in working_nct_id_0)
+    print('working_list: ' + str(working_nct_id_list))
+    print('Placeholders 1: ' + str(placeholders1))
     ########################################################################################################################
     # placeholders1 = ",".join("?" * 2000)
     # working_nct_id_0 = [record[0] for record in working_nct_id_list if record[2] == 0][0:2000]
@@ -182,10 +186,11 @@ def find_new_question(question_answer_list,working_nct_id_list, domain='all'):
     domain = domain.lower()
     conn = general_pool_criteria.connection()
     cur = conn.cursor()
-    if domain !='all':
+    if domain != 'all':
         table_name = 'dbo.all_criteria'
         placeholders2 = '?'
-        active_question_0 = [qa['question']['entity_text'] for qa in question_answer_list if qa['question']['domain'] == domain]
+        active_question_0 = [qa['question']['entity_text'] for qa in question_answer_list if
+                             qa['question']['domain'] == domain]
         placeholders3 = ",".join("?" * len(active_question_0))
         params = []
         params.extend(working_nct_id_0)
@@ -208,7 +213,7 @@ def find_new_question(question_answer_list,working_nct_id_list, domain='all'):
                     ) X
                     GROUP BY concept_name
                     ORDER BY sum(X.PlogP) DESC
-                ''' % (working_nct_id_0_len,working_nct_id_0_len,table_name, placeholders1,placeholders2)
+                ''' % (working_nct_id_0_len, working_nct_id_0_len, table_name, placeholders1, placeholders2)
         else:
             params.extend(active_question_0)
             sql = '''
@@ -227,10 +232,8 @@ def find_new_question(question_answer_list,working_nct_id_list, domain='all'):
                     ) X
                     GROUP BY concept_name
                     ORDER BY sum(X.PlogP) DESC
-                ''' % (working_nct_id_0_len,working_nct_id_0_len,table_name, placeholders1, placeholders2, placeholders3)
-
-
-        cur.execute(sql, params)
+                ''' % (working_nct_id_0_len, working_nct_id_0_len, table_name, placeholders1, placeholders2, placeholders3)
+        cur.execute(sql, [])
         next_concept = cur.fetchall()
         conn.close()
         cur.close()
@@ -242,8 +245,9 @@ def find_new_question(question_answer_list,working_nct_id_list, domain='all'):
         question_answer_list.append(this_q)
 
     else:
+        table_name = 'dbo.all_criteria'
         active_question_0 = [qa['question']['entity_text'] for qa in question_answer_list]
-        placeholders2 = ",".join("?" * len(active_question_0))
+        placeholders2 = ", ".join(str("'" + x + "'") for x in active_question_0)
         params = []
         params.extend(working_nct_id_0)
 
@@ -255,14 +259,14 @@ def find_new_question(question_answer_list,working_nct_id_list, domain='all'):
                         FROM
                             (
                         select CAST(count(distinct nct_id) AS [float]) AS count, concept_name, include, domain
-                            from dbo.all_criteria
+                            from %s
                             where nct_id in (%s) and concept_name is NOT NULL
                             group by concept_name, include, domain
                         ) X
                     ) X
                     GROUP BY concept_name, domain, include
                     ORDER BY sum(X.PlogP) DESC
-                ''' % (working_nct_id_0_len,working_nct_id_0_len, placeholders1)
+                ''' % (working_nct_id_0_len, working_nct_id_0_len, table_name, placeholders1)
         else:
             params.extend(active_question_0)
             sql = '''
@@ -272,7 +276,7 @@ def find_new_question(question_answer_list,working_nct_id_list, domain='all'):
                         FROM
                             (
                             select CAST(count(distinct nct_id) AS [float]) AS count, concept_name, include, domain
-                                from dbo.all_criteria
+                                from %s
                                 where nct_id in (%s) and concept_name is NOT NULL
                                 and concept_name not in (%s)
                                 group by concept_name, include, domain
@@ -280,8 +284,9 @@ def find_new_question(question_answer_list,working_nct_id_list, domain='all'):
                     ) X
                     GROUP BY concept_name, domain, include
                     ORDER BY sum(X.PlogP) DESC
-                ''' % (working_nct_id_0_len, working_nct_id_0_len, placeholders1, placeholders2)
-        cur.execute(sql, params)
+                ''' % (working_nct_id_0_len, working_nct_id_0_len, table_name, placeholders1, placeholders2)
+        print(sql)
+        cur.execute(sql, [])
         next_concept = cur.fetchall()
         conn.close()
         cur.close()
@@ -295,7 +300,7 @@ def find_new_question(question_answer_list,working_nct_id_list, domain='all'):
     return question_answer_list
 
 
-def find_nct_details(working_nct_id_list,npag):
+def find_nct_details(working_nct_id_list, npag):
     '''
     find nct details by connecting to AACT
     :param working_nct_id_list:
@@ -315,15 +320,16 @@ def find_nct_details(working_nct_id_list,npag):
         nct_details_for_this_page = []
     else:
         nct_id_this_page = [str(x) for x in nct_id_this_page]
-        placeholder = (tuple(nct_id_this_page),)
+        placeholder = ", ".join(str("'" + x + "'") for x in nct_id_this_page)
+        # placeholder = (tuple(nct_id_this_page))
         # nct_id_this_page = ['NCT02901717','NCT01287182']
         sql = '''
                 select c.nct_id,s.brief_title,c.name
                 from studies AS s
                 left join conditions AS c
                 on s.nct_id = c.nct_id
-                where s.nct_id in %s
-            '''%(placeholder)
+                where s.nct_id in (%s)
+            ''' % (placeholder)
         print(sql)
         conn = general_pool_aact.connection()
         cur = conn.cursor()
@@ -343,9 +349,11 @@ def find_nct_details(working_nct_id_list,npag):
             if r[0] not in nct_id_title.keys():
                 nct_id_title[r[0]] = r[1]
 
-        nct_details_for_this_page = [[nct_id,nct_id_rank[nct_id],nct_id_title[nct_id],nct_id_condition[nct_id]] for nct_id in nct_id_condition.keys()]
+        nct_details_for_this_page = [[nct_id, nct_id_rank[nct_id], nct_id_title[nct_id], nct_id_condition[nct_id]] for
+                                     nct_id in nct_id_condition.keys()]
         nct_details_for_this_page = sorted(nct_details_for_this_page, key=lambda x: x[1], reverse=False)
     return nct_details_for_this_page
+
 
 def find_size_of_active_trials(working_nct_id_list):
     '''
@@ -358,9 +366,10 @@ def find_size_of_active_trials(working_nct_id_list):
     size = len(working_nct_id_0)
     return size
 
+
 # working_nct_id_list = [['NCT02901717', 3431, 0], ['NCT01287182', 3432, 0],['NCT01035944', 3432, 0],['NCT00562068', 3431, 1], ['NCT00742300', 3431, 2]]
 # question_answer_list = [{'answer':{'include':'EXC'},'question': {'domain': 'condition', 'entity_text': 'pregnant'}} ]
-def update_working_nct_id_list(question_answer_list,working_nct_id_list):
+def update_working_nct_id_list(question_answer_list, working_nct_id_list):
     '''
     update working_nct_id_list by comparing question_answer_list with criteria knowledge base
     :param question_answer_list:
@@ -373,7 +382,7 @@ def update_working_nct_id_list(question_answer_list,working_nct_id_list):
     question_number = len(question_answer_list)
 
     if question_number > 0:
-        this_qa = question_answer_list[question_number-1]
+        this_qa = question_answer_list[question_number - 1]
         this_entity_text = this_qa['question']['entity_text']
         this_domain = this_qa['question']['domain']
         table_name = 'dbo.all_criteria'
@@ -394,56 +403,58 @@ def update_working_nct_id_list(question_answer_list,working_nct_id_list):
                 rangeend = this_answer['rangeend']
 
             if this_include == 'INC':
-                sql =   '''
-                        select distinct nct_id from %s
-                        where concept_name in ('%s')
-                        and
-                        (
-                            (include =0 and before_days >= %s)
-                            or
-                            (include =1 and before_days < %s)
-                        )
-                        ''' % (table_name,this_entity_text,rangeend,rangeend)
+                sql = '''
+                        select distinct nct_id
+                        from %s
+                        where (LOWER (concept_name) = LOWER ('%s') and
+                            is_exclusion = 0 and
+                            (1 = case
+                                when before_days != 0 and (%s > before_days) then 1
+                                    else 0
+                                end)) OR
+                            (lower(concept_name) = lower('%s') and
+                                is_exclusion = 1)
+                        ''' % (table_name, this_entity_text, rangeend, this_entity_text)
             else:
-                sql =   '''
-                        select distinct nct_id from %s
-                        where concept_name in ('%s')
-                        and
-                        (
-                            (include = 1)
-                        )
-                        ''' % (table_name,this_entity_text)
+                sql = '''
+                        select distinct nct_id
+                        from %s
+                        where lower(concept_name) = lower('%s') and
+                            is_exclusion = 0
+                        ''' % (table_name, this_entity_text)
         else:
             if 'measurement_value' in this_answer.keys() and this_include == 'INC':
                 measurement_value = this_answer['measurement_value']
-                sql =   '''
-                        select distinct nct_id from %s
-                        where concept_name in ('%s')
-                        and
-                        (
-                            (
-                                include = 0 and (min <= %s and max >= %s)
-                            ) or
-                            (
-                                include = 1 and (min > %s or max < %s)
-                            )
-                        )
-                        ''' % (table_name,this_entity_text,measurement_value,measurement_value,measurement_value,measurement_value)
+                sql = '''
+                        select distinct nct_id
+                        from %s
+                        where (lower(concept_name) = lower('%s') and
+                            %s <= numeric_att_max and
+                            %s >= numeric_att_min AND
+                            is_exclusion = 1) OR
+                            (lower(concept_name) = lower('%s') and
+                            (%s > numeric_att_max or
+                            %s < numeric_att_min) AND
+                            is_exclusion = 0)
+                        ''' % (table_name, this_entity_text, measurement_value, measurement_value, this_entity_text,
+                               measurement_value, measurement_value)
             else:
                 sql = '''
                     select top(0) nct_id from %s
-                '''% (table_name)
+                ''' % (table_name)
 
         print(sql)
         conn = general_pool_criteria.connection()
         cur = conn.cursor()
         cur.execute(sql)
         details = cur.fetchall()
+
+        filtered_nct_id = []
         filtered_nct_id = [nct_id[0] for nct_id in details]
         conn.close()
         cur.close()
         for nct_record in working_nct_id_list:
-            if nct_record[0] in filtered_nct_id and nct_record[2] == 0:
+            if nct_record[0] in filtered_nct_id:
                 nct_record[2] = question_number
         return working_nct_id_list
     else:
